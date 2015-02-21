477fb0b1ef381b979171bf098a47053f29567f70
elliotfiske <elliotfiske@gmail.com>
2015-02-20 16:57:59 -0800

    A new beginning.  A new hope.

---
+++ MonsterMe/MonsterMe/PlanetSceneController.swift
@@ -0,0 +1,87 @@
+//
+//  PlanetSceneController.swift
+//  MonsterMe
+//
+//  Created by Elliot Fiske on 2/20/15.
+//  Copyright (c) 2015 Monster Create. All rights reserved.
+//
+//
+//   Welcome to the planet scene controller!
+
+//   This is essentially the "root" view controller that every child view controller and scene
+//    will be parented to, if you go back far enough.
+//
+//   It maintains references to the Planet Model, which handles the current monster and props
+//    on the actual Planet Scene.
+//
+//   It handles all the user touch events to the planet scene, passing them to their appropriate
+//    model for processing.
+
+import UIKit
+import SpriteKit
+
+
+class PlanetSceneController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let scene = GameScene.unarchiveFromFile("PlanetScene") as? GameScene {
+            // Configure the view.
+            let skView = self.view as SKView
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            
+            /* We don't care how siblings order themselves.  Helps boost performance */
+            skView.ignoresSiblingOrder = true
+            
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .AspectFill
+            
+            skView.presentScene(scene)
+        }
+    }
+
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+
+    /** Set up autorotation */
+    override func supportedInterfaceOrientations() -> Int {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
+        } else {
+            return Int(UIInterfaceOrientationMask.All.rawValue)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
+
+
+
+//// This is just a pre-built extension that lets us use the .sks, basically Sprite Kit's
+////  equivalent of a storyboard.  We don't use it for anything other than
+////  initial setup of the game scene though.
+extension SKNode {
+    class func unarchiveFromFile(file : NSString) -> SKNode? {
+        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
+            var sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
+            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
+            
+            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as GameScene
+            archiver.finishDecoding()
+            return scene
+        } else {
+            return nil
+        }
+    }
+}
---
+++ MonsterMe/MonsterMeTests/MonsterMeTests.swift
@@ -0,0 +1,36 @@
+//
+//  MonsterMeTests.swift
+//  MonsterMeTests
+//
+//  Created by Elliot Fiske on 2/20/15.
+//  Copyright (c) 2015 Monster Create. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MonsterMeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
